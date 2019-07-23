47f93ba5a6f29d9de121391370c8560a1c095624
No33109 <15818470854@163.com>
2019-07-23 18:16:26 +0800

    Spring cloud setting

--- producer-demo/src/main/java/com/example/producer/demo/ProducerDemoApplication.java
+++ producer-demo/src/main/java/com/example/producer/demo/ProducerDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.producer.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProducerDemoApplication {
 
 	public static void main(String[] args) {
